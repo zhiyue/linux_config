b20673c23b48d08464a4c41c2624533ab4915901
ken <ken@appledembp.lan>
2013-11-18 08:10:23 +0800

    update install scripts

--- MakeFile.sh
+++ MakeFile.sh
@@ -3,16 +3,16 @@ cd .
 
 CF_DIR=`pwd`
 
-#git submodule init
-#git submodule update
 #更新配置的子模块到最新版本
+git submodule init
+git submodule update
 git submodule -q foreach git pull -q origin master
 
 #.bash_profile
-ln -sf $CF_DIR/aliases/bash_profile ~/.bash_profile
+ln -s $CF_DIR/aliases/bash_profile ~/.bash_profile
 
 # jump tool
-ln -sf $CF_DIR/aliases/markrc ~/.markrc
+ln -s $CF_DIR/aliases/markrc ~/.markrc
 
 #alias
 ALIASES_DIR="$CF_DIR/aliases"
@@ -20,12 +20,12 @@ ALIASES_FILES=`ls $ALIASES_DIR/*aliases`
 for f in $ALIASES_FILES
 do
     F_NAME=`basename $f`
-    ln -sf $f ~/"."$F_NAME
+    ln -s $f ~/"."$F_NAME
 done
 
 #dir_colors
-ln -sf $ALIASES_DIR/dir_colors ~/.dir_colors
-ln -sf $ALIASES_DIR/dircolors.256dark ~/.dircolors
+#ln -s dircolors.256dark ~/.dir_colors
+ln -s $CF_DIR/dircolors-solarized/dircolors.256dark .dir_colors
 
 #completion
 COMPLETION_DIR="$CF_DIR/completions"
@@ -33,22 +33,14 @@ ALIASES_FILES=`ls $COMPLETION_DIR/*completion`
 for f in $ALIASES_FILES
 do
     F_NAME=`basename $f`
-    ln -sf $f ~/"."$F_NAME
+    ln -s $f ~/"."$F_NAME
 done
 
-#vim config
-#ln -sf $CF_DIR/k-vim ~/.vim
-#if [ -L $CF_DIR/k-vim/vim ]
-#then
-#    rm $CF_DIR/k-vim/vim
-#fi
-#ln -sf $CF_DIR/k-vim/vimrc ~/.vimrc
 #自动安装配置vim
 sh -x $CF_DIR/k-vim/install.sh
 
-
 #bin
-ln -sf $CF_DIR/bin ~/bin
+ln -s $CF_DIR/bin ~/bin
 
 if [ ! -d ~/tmp ]
 then
@@ -74,6 +66,9 @@ then
 fi
 ln -s $CF_DIR/config/ssh-config ~/.ssh/config
 
-  #for tmux
+#for tmux
 #ln -s $CF_DIR/config/tmux-config ~/.tmux.conf
 ln -s $CF_DIR/config/tmux.conf ~/.tmux.conf
+
+#for git
+ln -s $CF_DIR/config/git-config ~/.gitconfig
